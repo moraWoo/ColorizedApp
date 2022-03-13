2fb10fb55d9c8141dd5f5295bcd4c39ce3c281cb
moraWoo <pkstratech@yandex.ru>
2022-03-14 00:52:08 +0500

    Setup layer

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redCount: UILabel!
+    @IBOutlet var greenCount: UILabel!
+    @IBOutlet var blueCount: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coloredView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func moveRedSlider(_ sender: UISlider) {
+        
+    }
+    @IBAction func moveGreenSlider(_ sender: UISlider) {
+        
+    }
+    @IBAction func moveBlueSlider(_ sender: UISlider) {
+        
+    }
 }
 
