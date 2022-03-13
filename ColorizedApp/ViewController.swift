f166e89386c5f1858467264e7a3a100a130acd32
moraWoo <pkstratech@yandex.ru>
2022-03-14 01:34:13 +0500

    Finish project

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -18,20 +18,45 @@ class ViewController: UIViewController {
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+
+    private var red: CGFloat = 0
+    private var green: CGFloat = 0
+    private var blue: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         coloredView.layer.cornerRadius = 10
+        
+        redCount.text = String(format:"%.2f", redSlider.value)
+        greenCount.text = String(format:"%.2f", greenSlider.value)
+        blueCount.text = String(format:"%.2f", blueSlider.value)
+        
+        red = CGFloat(redSlider.value)
+        green = CGFloat(greenSlider.value)
+        blue = CGFloat(blueSlider.value)
+               
+        colorizeView()
     }
 
-    @IBAction func moveRedSlider(_ sender: UISlider) {
-        
+    @IBAction func moveRedSlider() {
+        redCount.text = String(format:"%.2f", redSlider.value)
+        red = CGFloat(redSlider.value)
+        colorizeView()
     }
-    @IBAction func moveGreenSlider(_ sender: UISlider) {
-        
+    @IBAction func moveGreenSlider() {
+        greenCount.text = String(format:"%.2f", greenSlider.value)
+        green = CGFloat(greenSlider.value)
+        colorizeView()
     }
-    @IBAction func moveBlueSlider(_ sender: UISlider) {
-        
+    @IBAction func moveBlueSlider() {
+        blueCount.text = String(format:"%.2f", blueSlider.value)
+        blue = CGFloat(blueSlider.value)
+        colorizeView()
     }
+    
+    private func colorizeView() {
+        coloredView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
 }
 
